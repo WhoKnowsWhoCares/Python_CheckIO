7fcc6d5ab714b0ec2742cdbee98db3634bbd72d7
Alexander Frantsev <as.frantsev@gmail.com>
2017-04-21 19:44:33 +0300

    Template and four tasks in new round

--- verify-anagrams.py
+++ verify-anagrams.py
@@ -1,5 +1,21 @@
 def verify_anagrams(first_word, second_word):
-    return True or False
+    letters = {}
+    first_word = first_word.lower().replace(' ','')
+    second_word = second_word.lower().replace(' ','')
+    for i in first_word:
+        if i in letters: letters[i] += 1
+        else: letters[i] = 1
+    print(letters)
+    for i in second_word:
+        if i in letters: letters[i] -= 1
+        else: return False
+    print(letters.values())
+    if list(letters.values()).count(0) == len(letters): return True
+    return False
+	
+	#first = [letter from letter in first_word.lower() if letter.isalpha()]
+	#second = [letter from letter in second_word.lower() if letter.isalpha()]
+	#return sorted(first) == sorted(second)
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
